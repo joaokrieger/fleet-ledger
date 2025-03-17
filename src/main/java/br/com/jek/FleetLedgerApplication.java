638f0c2a09b584a7d0942d16baf3808e64ed39cb
JEK <joaoeduardokrieger123@gmail.com>
2025-03-16 21:04:13 -0300

    Initializing Spring Boot Application

---
+++ src/main/java/br/com/jek/FleetLedgerApplication.java
@@ -0,0 +1,13 @@
+package br.com.jek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FleetLedgerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FleetLedgerApplication.class, args);
+	}
+
+}
